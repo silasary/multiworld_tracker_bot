45729ac6877b8e6e0ea6ffbf6c2086b9f5a28b37
Katelyn Gigante <clockwork.singularity@gmail.com>
2024-11-03 20:43:00 +1100

    And fix

--- ap_alert/converter.py
+++ ap_alert/converter.py
@@ -5,4 +5,4 @@ import cattrs
 converter = cattrs.Converter()
 
 converter.register_structure_hook(datetime.datetime, lambda x, *_: datetime.datetime.fromisoformat(x) if x else None)
-converter.register_unstructure_hook(datetime.datetime, lambda x, *_: x.isoformat() if isinstance(x, datetime.timedelta) else None)
+converter.register_unstructure_hook(datetime.datetime, lambda x, *_: x.isoformat() if x else None)
