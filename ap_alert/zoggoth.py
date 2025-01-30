bc4f7fef7da36c36b62d5ca60c5977f0ebe813c5
Katelyn Gigante <clockwork.singularity@gmail.com>
2025-01-30 17:52:55 +1100

    Lower the bar

--- ap_alert/zoggoth.py
+++ ap_alert/zoggoth.py
@@ -93,7 +93,7 @@ def load_datapackage(name: str, dp: Datapackage) -> None:
 
     if written:
         quoted_name = name.replace(" ", "_")
-        if len(to_append) > 10:
+        if len(to_append) >= 5:
             fresh = False
         if fresh:
             subprocess.run(["git", "branch", "-f", quoted_name], cwd="zoggoth_repo")
